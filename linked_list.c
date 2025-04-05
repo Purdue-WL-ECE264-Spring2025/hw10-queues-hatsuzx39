41615ac4bf275689bdf2da92544207b98ffa1b47
hatsuzx39 <chantel.zixin.sun@gmail.com>
2025-04-04 21:03:29 -0400

    aaa

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,82 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+  struct list_node *node = malloc(sizeof(struct list_node));
+  if(node){
+    node->value = value;
+    node->next = NULL;
+  }
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+  return node;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *node = new_node(value);
+  if(!node){
+    return;
+  }
+  node->next = list->head;
+  list->head = node;
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node *node = new_node(value);
+  if(!node){
+    return;
+  }
+  if (list->head == NULL){
+    list->head = node;
+  }
+  else{
+    struct list_node *cur = list->head;
+    while (cur->next != NULL){
+      cur = cur->next;
+    }
+    cur->next = node;
+  }
+}
 
-void free_list(struct linked_list list) {}
+size_t remove_from_head(struct linked_list *list) { 
+  if(list->head ==NULL){
+    return 0;
+  }
+  struct list_node *temp = list->head;
+  size_t value = temp->value;
+  list->head = temp->next;
+  free(temp);
+  return value;
+}
+
+size_t remove_from_tail(struct linked_list *list) {
+  if(list->head == NULL){
+    return 0;
+  }
+  if(list->head->next == NULL){
+    size_t value = list->head->value;
+    free(list->head);
+    list->head = NULL;
+    return value;
+  }
+  struct list_node *cur = list->head;
+  while(cur->next->next != NULL){
+    cur = cur->next;
+  }
+  size_t value = cur->next->value;
+  free(cur->next);
+  cur->next = NULL;
+  return value;
+}
+
+void free_list(struct linked_list list) {
+  struct list_node *cur = list.head;
+  while(cur){
+    struct list_node *next = cur->next;
+    free(cur);
+    cur = next;
+  }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,162 @@
 #include "queue.h"
 #include "tile_game.h"
+#include "linked_list.h"
+#include <stdlib.h>
+#define VISITED_SIZE 100003
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+    uint64_t ser = serialize(state);
+    insert_at_tail(&q->data, (size_t)ser);
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) { 
+    uint64_t ser = remove_from_head(&q->data);
+    return deserialize(ser);
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) {
+    int goal[4][4] = {
+        {1,  2,  3,  4},
+        {5,  6,  7,  8},
+        {9,  10, 11, 12},
+        {13, 14, 15, 0}
+    };
+    //check if start state is goal
+    int is_goal = 1;
+    for (int i = 0; i < 4 && is_goal; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (start.tiles[i][j] != goal[i][j]) {
+                is_goal = 0;
+                break;
+            }
+        }
+    }
+    if (is_goal){
+        return start.num_steps;
+    }
+    struct queue q;
+    q.data.head=NULL;
+
+    uint64_t *visited_table = calloc(VISITED_SIZE, sizeof(uint64_t));
+    char *visited_flag = calloc(VISITED_SIZE, sizeof(char));
+    //mark start state  as visited
+    uint64_t start_ser = serialize(start);
+    int idx = start_ser % VISITED_SIZE;
+    while (visited_flag[idx]){
+        idx = (idx + 1) % VISITED_SIZE;
+    }
+    visited_table[idx] = start_ser;
+    visited_flag[idx] = 1;
+
+    enqueue(&q,start);
+
+    //BFS
+    while (q.data.head != NULL){
+        struct game_state cur = dequeue(&q);
+        int cur_is_goal = 1;
+        for(int i =0; i<4 && cur_is_goal; i++){
+            for(int j =0; j<4; j++){
+                if (cur.tiles[i][j] != goal[i][j]){
+                    cur_is_goal = 0;
+                    break;
+                }
+            }
+        }
+        if (cur_is_goal){
+            int moves = cur.num_steps;
+            free(visited_table);
+            free(visited_flag);
+            return moves;
+
+        }
+
+        //move up
+        //valid if empty_row <3
+        if(cur.empty_row <3){
+            struct game_state next = cur;
+            move_up(&next);
+            uint64_t ser = serialize(next);
+            int found = 0;
+            idx = ser % VISITED_SIZE;
+            while(visited_flag[idx]){
+                if (visited_table[idx] == ser){
+                    found = 1;
+                    break;
+                }
+                idx = (idx+1)%VISITED_SIZE;
+            }
+            if(!found){
+                visited_table[idx] = ser;
+                visited_flag[idx] = 1;
+                enqueue(&q, next);
+            }
+        }
+        //move down
+        //valid if emptyrow >0
+        if(cur.empty_row>0){
+            struct game_state next = cur;
+            move_down(&next);
+            uint64_t ser = serialize(next);
+            int found = 0;
+            idx = ser % VISITED_SIZE;
+            while (visited_flag[idx]){
+                if(visited_table[idx]==ser){
+                    found = 1;
+                    break;
+                }
+                idx = (idx+1) % VISITED_SIZE;
+            }
+            if (!found){
+                visited_table[idx] = ser;
+                visited_flag[idx] = 1;
+                enqueue(&q, next);
+            }
+
+        }
+        //move left
+        // valid if empty_col <3
+        if(cur.empty_col <3){
+            struct game_state next = cur;
+            move_left(&next);
+            uint64_t ser = serialize(next);
+            int found = 0;
+            idx = ser%VISITED_SIZE;
+            while (visited_flag[idx]){
+                if(visited_table[idx] == ser){
+                    found = 1;
+                    break;
+                }
+                idx = (idx+1) %VISITED_SIZE;
+            }
+            if(!found){
+                visited_table[idx] = ser;
+                visited_flag[idx] =1;
+                enqueue(&q, next);
+            }
+        }
+        //move rigjt if emptycol >0
+        if(cur.empty_col <3){
+            struct game_state next = cur;
+            move_right(&next);
+            uint64_t ser = serialize(next);
+            int found = 0;
+            idx = ser%VISITED_SIZE;
+            while (visited_flag[idx]){
+                if(visited_table[idx] == ser){
+                    found = 1;
+                    break;
+                }
+                idx = (idx+1) %VISITED_SIZE;
+            }
+            if(!found){
+                visited_table[idx] = ser;
+                visited_flag[idx] =1;
+                enqueue(&q, next);
+            }
+        }
+    }
+    free(visited_table);
+    free(visited_flag);
+    return -1; //no solution found
+
+}
